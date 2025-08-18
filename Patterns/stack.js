ca28ce0816fb4d2a225aef3b571c652e7d8afdb0
DileepChittamuru <chdileep8500@gmail.com>
2025-08-18 13:48:27 +0530

    Update stack.js

--- Patterns/stack.js
+++ Patterns/stack.js
@@ -1,1 +1,21 @@
+function validParanthesis(str) {
+    let obj = {
+    ')' : '(',
+    '}' : '{',
+    ']' : '['
+  }
+  let stack = []
+  for (let val of str) {
+    if (val in obj) {
+      const ele = stack.length ? stack.pop(): '#'
+      if(ele !== obj[val]) {
+        return false
+      }
+    } else {
+      stack.push(val)
+    }
+  }
+  return stack.length === 0;
+}
 
+console.log(validParanthesis('([{}])'))
