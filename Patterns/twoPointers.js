0d9f340005eaa195d4f6fb033a8df7ca930dc63a
DileepChittamuru <chdileep8500@gmail.com>
2025-08-24 11:57:46 +0530

    Update twoPointers.js

--- Patterns/twoPointers.js
+++ Patterns/twoPointers.js
@@ -1,1 +1,15 @@
 
+function twoPointers() {
+  // Move zero's to end
+  function moveZeroToEnd(arr) {
+    let left = 0;
+    for (let right = 0; right < arr.length; right++) {
+      if(arr[right] !== 0) {
+        [arr[left], arr[right]] = [arr[right], arr[left]];
+        left++;
+      }
+    }
+    return arr;
+  }
+  console.log(moveZeroToEnd([1,0,2,0,3,0]))
+}
