d0539fad9228dec9d3e215109ce57325ac3f9431
DileepChittamuru <chdileep8500@gmail.com>
2025-08-25 12:53:44 +0530

    Update twoPointers.js

--- Patterns/twoPointers.js
+++ Patterns/twoPointers.js
@@ -18,7 +18,7 @@ function twoPointers() {
     }
     return true
   }
-  
+  // Move Zeros to end
   function moveZeroToEnd(arr) {
     let left = 0;
     for (let right = 0; right < arr.length; right++) {
@@ -30,4 +30,18 @@ function twoPointers() {
     return arr;
   }
   console.log(moveZeroToEnd([1,0,2,0,3,0]))
+
+  // Buy and Sell 
+  function buyAndSell(arr) {
+    let left = 0;
+    let profit = 0;
+    for(let right=1; right<arr.length;  right++) {
+      if (arr[right] > arr[left]) {
+        profit = Math.max(profit, arr[right] - arr[left])
+      } else {
+        left = right;
+      }
+    }
+    return profit;
+  }
 }
