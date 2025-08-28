95f7af99f43642139833f714baa2e271d7d3a5c8
DileepChittamuru <chdileep8500@gmail.com>
2025-08-28 20:49:22 +0530

    Update recursive.js

--- Patterns/recursive.js
+++ Patterns/recursive.js
@@ -24,4 +24,32 @@ function recursive() {
       }
     }
     recursiveObj(obj)
+   // deep clone
+   function deepClone(value) {
+        if (value === null || typeof value !== 'object') {
+          return value;
+        }
+        if (Array.isArray(value)) {
+          return value.map(item => deepClone(item))
+        }
+        if (typeof value === 'object') {
+          let newObj = {};
+          for (let key in value) {
+            newObj[key] = deepClone(value[key])
+          }
+          return newObj;
+        }
+      }
+      
+      const obj = {
+        a: 1,
+        b: 2,
+        c: {
+          d: 3,
+          e: 4,
+        },
+        f: [5, 6],
+      };
+
+      console.log(deepClone(obj))
 }
